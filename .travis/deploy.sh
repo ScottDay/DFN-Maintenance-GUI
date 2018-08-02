33be155fb69cbd6201ea555b2332e7f0df324c0f
ScottDay <17160182@student.curtin.edu.au>
2018-08-02 14:18:47 +1000

    chore(travis): fix docker container registry reference

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,13 +3,13 @@
 # Set an option to exit immediately if any error appears
 set -o errexit
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
 
 # Stage the updated version and submodules.
 git add .env DFN-Maintenance-GUI-Backend DFN-Maintenance-GUI-Frontend DFN-Maintenance-GUI-Config
 
 if [[ "$REQUEST_TYPE" == "release" ]]; then
-    docker push $RELEASE_DOCKER_IMAGE_NAME
+    docker push scottydevil/dfn-maintenance-gui:$RELEASE_DOCKER_IMAGE_NAME
 
     # Commit the updated .env file.
     git commit -m "[skip ci] $RELEASE_DOCKER_IMAGE_NAME"
@@ -30,7 +30,7 @@ elif [[ "$REQUEST_TYPE" == "dev" ]]; then
     git tag $DEV_DOCKER_IMAGE_NAME
 fi
 
-docker push $DEV_DOCKER_IMAGE_NAME
+docker push scottydevil/dfn-maintenance-gui:$DEV_DOCKER_IMAGE_NAME
 
 # Push commit and tags.
 git push --follow-tags
--- .travis/script.sh
+++ .travis/script.sh
@@ -4,7 +4,7 @@
 set -o errexit
 
 if [[ "$REQUEST_TYPE" == "release" ]]; then
-    docker build --file .docker/prod/Dockerfile -t /DFN-Maintenance-GUI:$RELEASE_DOCKER_IMAGE_NAME .
+    docker build --file .docker/prod/Dockerfile -t scottydevil/dfn-maintenance-gui:$RELEASE_DOCKER_IMAGE_NAME .
 fi
 
-docker build --file .docker/dev/Dockerfile -t /DFN-Maintenance-GUI:$DEV_DOCKER_IMAGE_NAME .
+docker build --file .docker/dev/Dockerfile -t scottydevil/dfn-maintenance-gui:$DEV_DOCKER_IMAGE_NAME .
