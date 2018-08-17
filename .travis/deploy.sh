4c7550534391f586c1a6479d0d0adda85e6cc43a
ScottDay <17160182@student.curtin.edu.au>
2018-08-17 16:27:11 +1000

    feat(docker): backend config through command line arguments

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,10 +3,11 @@
 docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
 
 # Stage the updated version and submodules.
-git add .env DFN-Maintenance-GUI-Backend DFN-Maintenance-GUI-Frontend DFN-Maintenance-GUI-Config
+git add .env DFN-Maintenance-GUI-Frontend DFN-Maintenance-GUI-Backend DFN-Maintenance-GUI-Config
 
 if [[ "$REQUEST_TYPE" == "release" ]]; then
-    docker push scottydevil/dfn-maintenance-gui:v$RELEASE_VERSION scottydevil/dfn-maintenance-gui:latest
+    # Push docker tags.
+    docker push scottydevil/dfn-maintenance-gui:latest
 
     # Commit the updated .env file.
     git commit -m "v$RELEASE_VERSION $BUILD_DATE $BUILD_DATE"$'\n\n''[skip ci]' 
@@ -16,6 +17,10 @@ if [[ "$REQUEST_TYPE" == "release" ]]; then
 else
     # TODO: Remove dev docker images from docker hub.
     
+    # Push docker tags.
+    docker push scottydevil/dfn-maintenance-gui:v$RELEASE_VERSION.$DEV_VERSION 
+    docker push scottydevil/dfn-maintenance-gui:dev
+
     # Remove old dev git tags from the last release.
     git push origin --delete $(git tag -l "v*.*")
 
@@ -26,7 +31,7 @@ else
     git tag "v$RELEASE_VERSION.$DEV_VERSION"
 fi
 
-docker push scottydevil/dfn-maintenance-gui:v$RELEASE_VERSION scottydevil/dfn-maintenance-gui:v$RELEASE_VERSION.$DEV_VERSION scottydevil/dfn-maintenance-gui:dev
+docker push scottydevil/dfn-maintenance-gui:v$RELEASE_VERSION 
 
 # Push commit and tags.
 git push origin HEAD:$TRAVIS_BRANCH
--- .travis/script.sh
+++ .travis/script.sh
@@ -11,15 +11,21 @@ mkdir build
 mkdir build/db
 cp -r DFN-Maintenance-GUI-Backend/main.py build/
 cp -r DFN-Maintenance-GUI-Backend/requirements/prod.txt build/requirements.txt
-cp -r DFN-Maintenance-GUI-Config/auth.db build/db/
 cp -r DFN-Maintenance-GUI-Backend/src build/src
 cp -r DFN-Maintenance-GUI-Frontend/dist build/dist
+
+# Copy database.
+if [[ "$REQUEST_TYPE" == "release" ]]; then
+    cp -r DFN-Maintenance-GUI-Config/auth.db build/db/
+else
+    cp -r DFN-Maintenance-GUI-Config/dev.db build/db/
+fi
 
 # Docker build.
 if [[ "$REQUEST_TYPE" == "release" ]]; then
     docker build -t scottydevil/dfn-maintenance-gui:v$RELEASE_VERSION -t scottydevil/dfn-maintenance-gui:latest .
 else
-    docker build -t scottydevil/dfn-maintenance-gui:v$RELEASE_VERSION -t scottydevil/dfn-maintenance-gui:v$RELEASE_VERSION.$DEV_VERSION -t scottydevil/dfn-maintenance-gui:dev .
+    docker build -f .docker/Dockerfile -t scottydevil/dfn-maintenance-gui:v$RELEASE_VERSION -t scottydevil/dfn-maintenance-gui:v$RELEASE_VERSION.$DEV_VERSION -t scottydevil/dfn-maintenance-gui:dev .
 fi
 
 rm -rf build
