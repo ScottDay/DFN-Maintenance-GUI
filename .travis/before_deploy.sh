95e4d3bf5434c602d782f28e8091dca0308a8d40
Scott Day <17160182@student.curtin.edu.au>
2018-08-31 17:23:37 +1000

    fix(travis): github release body formatting
    [skip ci]

--- .travis/before_deploy.sh
+++ .travis/before_deploy.sh
@@ -4,8 +4,8 @@
 set -o errexit
 
 # Setup release body message.
-curl -s https://api.github.com/repos/ScottDay/DFN-Maintenance-GUI-Frontend/releases/latest | jq -r '.body' | cut -c 3- | (printf "# Frontend " && cat) > frontend.txt
-curl -s https://api.github.com/repos/ScottDay/DFN-Maintenance-GUI-Backend/releases/latest | jq -r '.body' | (printf "# Backend\n\n" && cat) > backend.txt
+curl -s https://api.github.com/repos/ScottDay/DFN-Maintenance-GUI-Frontend/releases/latest | jq -r '.body' | cut -c 3- | (printf "# Frontend " && cat) > frontend.md
+curl -s https://api.github.com/repos/ScottDay/DFN-Maintenance-GUI-Backend/releases/latest | jq -r '.body' | cut -c 3- | (printf "# Backend " && cat) > backend.md
 
 export RELEASE_TAG="v$RELEASE_VERSION"
-export RELEASE_BODY="$(cat frontend.txt backend.txt)"
+export RELEASE_BODY="$(cat frontend.md backend.md)"
