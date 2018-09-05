735e54b4d56fd67e581ab46d879ad36671212198
Scott Day <17160182@student.curtin.edu.au>
2018-09-05 18:53:08 +1000

    feat(travis): github release assets
    [skip ci]

--- .travis/deploy/after_deploy.sh
+++ .travis/deploy/after_deploy.sh
@@ -11,10 +11,27 @@ if [ "$REQUEST_TYPE" == "release" ]; then
         "prerelease": false
     }'
 
-    curl -s -X PATCH \
+    upload_url=$(curl -s -X PATCH \
         -H "Content-Type: application/json" \
         -H "Accept: application/json" \
         -H "Authorization: token ${GH_TOKEN}" \
         -d "$BODY" \
-        "https://api.github.com/repos/ScottDay/DFN-Maintenance-GUI/releases/tag/$RELEASE_TAG"
+        "https://api.github.com/repos/ScottDay/DFN-Maintenance-GUI/releases/tag/$RELEASE_TAG" | jq -r '.upload_url')
+    
+    mv build/ GUI/
+    tar -czfv GUI.tar.gz GUI/*
+
+    curl -s \
+        -H "Content-Type: application/gzip" \
+        -H "Authorization: token ${GH_TOKEN}"  \
+        --data-binary @GUI.tar.gz  \
+        "$upload_url?name=GUI.tar.gz&label=GUI.tar.gz"
+    
+    tar -czfv Installer.tar.gz DFN-Maintenance-GUI-Installer/*
+
+    curl -s \
+        -H "Content-Type: application/gzip" \
+        -H "Authorization: token ${GH_TOKEN}"  \
+        --data-binary @Installer.tar.gz  \
+        "$upload_url?name=Installer.tar.gz&label=Installer.tar.gz"
 fi
