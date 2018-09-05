bf1e3524f0d6c21e4823b14ab8a175029f3b6df9
Scott Day <17160182@student.curtin.edu.au>
2018-09-05 19:59:04 +1000

    fix(travis): after deploy script tar build dir
    [skip ci]

--- .travis/deploy/after_deploy.sh
+++ .travis/deploy/after_deploy.sh
@@ -1,37 +1,49 @@
 #!/bin/bash
 
+# Set an option to exit immediately if any error appears.
+set -o errexit
+
 # Give GitHub release a body describing the submodules changes.
-if [ "$REQUEST_TYPE" == "release" ]; then
-    BODY='{
-        "tag_name": "$RELEASE_TAG",
-        "target_commitish": "master",
-        "name": "$RELEASE_TAG",
-        "body": "$RELEASE_BODY",
-        "draft": false,
-        "prerelease": false
-    }'
+mv build/ GUI/
+tar -czvf GUI.tar.gz GUI/
 
-    upload_url=$(curl -s -X PATCH \
-        -H "Content-Type: application/json" \
-        -H "Accept: application/json" \
-        -H "Authorization: token ${GH_TOKEN}" \
-        -d "$BODY" \
-        "https://api.github.com/repos/ScottDay/DFN-Maintenance-GUI/releases/tag/$RELEASE_TAG" | jq -r '.upload_url')
-    
-    mv build/ GUI/
-    tar -czfv GUI.tar.gz GUI/*
+mv DFN-Maintenance-GUI-Installer/ Installer/
+tar -czvf Installer.tar.gz Installer/
 
-    curl -s \
-        -H "Content-Type: application/gzip" \
-        -H "Authorization: token ${GH_TOKEN}"  \
-        --data-binary @GUI.tar.gz  \
-        "$upload_url?name=GUI.tar.gz&label=GUI.tar.gz"
-    
-    tar -czfv Installer.tar.gz DFN-Maintenance-GUI-Installer/*
+request_body='{
+	"tag_name": "",
+    "target_commitish": "master",
+    "name": "",
+    "body": "",
+    "draft": false,
+    "prerelease": false
+}'
 
-    curl -s \
-        -H "Content-Type: application/gzip" \
-        -H "Authorization: token ${GH_TOKEN}"  \
-        --data-binary @Installer.tar.gz  \
-        "$upload_url?name=Installer.tar.gz&label=Installer.tar.gz"
-fi
+request_body=$(echo $request_body | jq ". + {"tag_name": \"$RELEASE_TAG\", "name": \"$RELEASE_TAG\", "body": \"$RELEASE_BODY\"}")
+
+release_id=$(curl -s \
+    -X GET \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -H "Authorization: token ${GH_TOKEN}" \
+    "https://api.github.com/repos/ScottDay/DFN-Maintenance-GUI/releases/latest" | jq -r '.id')
+
+curl -s \
+    -X PATCH \
+    -H "Content-Type: application/json" \
+    -H "Accept: application/json" \
+    -H "Authorization: token ${GH_TOKEN}" \
+    -d "$request_body" \
+    "https://api.github.com/repos/ScottDay/DFN-Maintenance-GUI/releases/$release_id"
+
+curl -s \
+    -H "Content-Type: application/gzip" \
+    -H "Authorization: token ${GH_TOKEN}" \
+    --data-binary @GUI.tar.gz \
+    "https://uploads.github.com/repos/ScottDay/DFN-Maintenance-GUI/releases/$release_id/assets?name=GUI.tar.gz&label=GUI.tar.gz"
+
+curl -s \
+    -H "Content-Type: application/gzip" \
+    -H "Authorization: token ${GH_TOKEN}" \
+    --data-binary @Installer.tar.gz \
+    "https://uploads.github.com/repos/ScottDay/DFN-Maintenance-GUI/releases/$release_id/assets?name=Installer.tar.gz&label=Installer.tar.gz"
--- .travis/deploy/before_deploy.sh
+++ .travis/deploy/before_deploy.sh
@@ -4,9 +4,9 @@
 set -o errexit
 
 # Setup release body message.
-curl -s https://api.github.com/repos/ScottDay/DFN-Maintenance-GUI-Frontend/releases/latest | jq -r '.body' | cut -c 3- | (printf "# Frontend " && cat) > frontend.md
-curl -s https://api.github.com/repos/ScottDay/DFN-Maintenance-GUI-Backend/releases/latest | jq -r '.body' | cut -c 3- | (printf "# Backend " && cat) > backend.md
-curl -s https://api.github.com/repos/ScottDay/DFN-Maintenance-GUI-Installer/releases/latest | jq -r '.body' | cut -c 3- | (printf "# Installer " && cat) > installer.md
+frontend=$(curl -s https://api.github.com/repos/ScottDay/DFN-Maintenance-GUI-Frontend/releases/latest | jq -r '.body' | cut -c 3- | (printf "# Frontend " && cat))
+backend=$(curl -s https://api.github.com/repos/ScottDay/DFN-Maintenance-GUI-Backend/releases/latest | jq -r '.body' | cut -c 3- | (printf "# Backend " && cat))
+installer=$(curl -s https://api.github.com/repos/ScottDay/DFN-Maintenance-GUI-Installer/releases/latest | jq -r '.body' | cut -c 3- | (printf "# Installer " && cat))
 
 export RELEASE_TAG="v$RELEASE_VERSION"
-export RELEASE_BODY="$(cat frontend.md backend.md installer.md)"
+export RELEASE_BODY="$frontend\n$backend\n$installer"
